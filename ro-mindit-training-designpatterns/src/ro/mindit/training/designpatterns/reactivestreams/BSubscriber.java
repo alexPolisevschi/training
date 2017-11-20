4360b2ad490e7c66c0cbbf8427620548585185a2
Alexandra Polisevschi <alexandra.polisevschi@mindit.ro>
2017-11-20 02:08:40 +0200

    added reactive streams example

--- ro-mindit-training-designpatterns/src/ro/mindit/training/designpatterns/reactivestreams/BSubscriber.java
+++ ro-mindit-training-designpatterns/src/ro/mindit/training/designpatterns/reactivestreams/BSubscriber.java
@@ -10,12 +10,12 @@ import java.util.concurrent.Flow;
  *
  * There are many alternatives of Observer design pattern and Reactive Streams is one of them.
  */
-public class TestSubscriber<T> implements Flow.Subscriber<T> {
+public class BSubscriber implements Flow.Subscriber<String> {
 
     // Subscription: a class that is used to control the flow of messages between Subscriber and the Publisher
     private Flow.Subscription subscription;
 
-    public List<T> consumedElements = new LinkedList<>();
+    private List<String> consumedElements = new LinkedList<>();
 
 
     // called before processing starts
@@ -27,8 +27,11 @@ public class TestSubscriber<T> implements Flow.Subscriber<T> {
 
     // called whenever the Publisher publishes a new message
     @Override
-    public void onNext(T item) {
-        System.out.println("Got : " + item);
+    public void onNext(String item) {
+        if (item.contains("bomb") ) {
+            System.out.println("B received a bomb alert! Investigating... " + item);
+            consumedElements.add(item);
+        }
         subscription.request(1);
     }
 
@@ -41,6 +44,6 @@ public class TestSubscriber<T> implements Flow.Subscriber<T> {
     // called when the Publisher is closed
     @Override
     public void onComplete() {
-        System.out.println("Done");
+        System.out.println("B: processed " + consumedElements.size() + " elements");
     }
 }
